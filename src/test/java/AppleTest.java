4afa8084fab9b45f8e9b40405ee0935012dc3d67
koskv <koskv@list.ru>
2023-01-10 18:47:55 +0300

    Initial commit

---
+++ src/test/java/AppleTest.java
@@ -0,0 +1,16 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+
+public class AppleTest {
+
+    @Test
+    @DisplayName("Check report allure")
+    public void checkHref() {
+        int i = 1;
+        Assert.assertEquals(1,i);
+
+    }
+}
